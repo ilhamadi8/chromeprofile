528563fd0c1d8723b62a3add153b72988f8ef18f
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-27 10:36:12 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -5,7 +5,7 @@ BASE_DIR="$HOME/.config/google-chrome"
 
 # Create profiles
 for i in {1..30}; do
-    PROFILE_DIR="${BASE_DIR}/Profile ${i}"
+    PROFILE_DIR="${BASE_DIR}/profile${i}"
     mkdir -p "$PROFILE_DIR"
     google-chrome --user-data-dir="$PROFILE_DIR" --profile-directory="Profile ${i}" &
     sleep 2 # Give Chrome some time to initialize the profile
