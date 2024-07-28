71e8463fd97348cc300eff8add93b8f27eb764f0
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-29 00:59:42 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,11 +1,11 @@
 BASE_DIR="$HOME/.config/google-chrome"
 SOURCE_PROFILE="profile1"
 DESTINATION_PREFIX="${BASE_DIR}/profile"
-START_INDEX=2
+START_INDEX=1
 END_INDEX=20
 
 for i in $(seq $START_INDEX $END_INDEX); do
-    google-chrome --profile-directory="profile$>
+    google-chrome --profile-directory="profile$i" &>/dev/null &
 done
 
 echo "Profiles open."
